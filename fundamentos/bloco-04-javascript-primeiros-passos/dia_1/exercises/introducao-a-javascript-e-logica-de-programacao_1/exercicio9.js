ae22064ac0a221a09122a040d07b3e7fa19a0386
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-23 20:38:08 -0300

    Exercicio 9 - Completo

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio9.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 10;
+let c = 10;
+
+if(a % 2 !== 0) {
+    console.log(true)
+}
+else if(b % 2 !== 0){
+    console.log(true)
+}
+else if(c % 2 !== 0){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
