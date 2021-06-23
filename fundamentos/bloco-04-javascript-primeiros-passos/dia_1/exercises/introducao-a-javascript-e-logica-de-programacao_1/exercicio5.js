8cd2f6db6ea7cb60461cdbb72ff2ae4060023fd2
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-23 18:46:51 -0300

    Exercicio 5 - Completo

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio5.js
@@ -0,0 +1,14 @@
+let a = 50
+let b = 50
+let c = 80
+let soma = a + b + c === 180;
+
+if(soma) {
+    console.log(true)
+}
+else if(soma) {
+    console.log(false)
+}
+else {
+    console.log("ERRO: Input invalido")
+}
