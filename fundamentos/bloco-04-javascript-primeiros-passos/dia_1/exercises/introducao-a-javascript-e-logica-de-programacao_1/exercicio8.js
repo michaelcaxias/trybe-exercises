916b02fc644306f5bd7edf0c0cc7c4cd3266bb6c
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-23 20:32:36 -0300

    Exercicio 8 - Completo

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio8.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 5;
+let c = 10;
+
+if(a % 2 === 0) {
+    console.log(true)
+}
+else if(b % 2 === 0){
+    console.log(true)
+}
+else if(c % 2 === 0){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
