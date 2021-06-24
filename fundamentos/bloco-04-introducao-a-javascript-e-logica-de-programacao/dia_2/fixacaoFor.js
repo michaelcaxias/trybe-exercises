bff81f7f333cdac0824634fea46846545193a239
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-24 15:34:30 -0300

    Adiciona Fixação para For e For/of

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_2/fixacaoFor.js
@@ -0,0 +1,14 @@
+//Fixacao For
+
+//ex1 
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+//    console.log(groceryList[index])
+}
+//ex2 - for/of
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let element of names){
+    console.log(element)
+}
