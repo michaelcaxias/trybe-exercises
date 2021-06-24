8464b38a29fd4a96af305cb46ad6a90b63e39f8f
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-24 19:43:34 -0300

    Exercicio 5 - Completo

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
@@ -3,17 +3,25 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //ex 02
 let sum = 0;
 for (let index = 0; index < numbers.length; index += 1) {
-    sum += numbers[index]
-    console.log(sum)
+    sum += numbers[index];
+    console.log(sum);
 }
 //ex 03
-let arithmeticAverage = sum / numbers.length
-console.log("A média aritmética é: " + arithmeticAverage)
+let arithmeticAverage = sum / numbers.length;
+console.log("A média aritmética é: " + arithmeticAverage);
 //ex 04
 if(arithmeticAverage > 20){
-    console.log("valor maior que 20")
+    console.log("valor maior que 20");
 }
 else{
-    console.log("valor menor ou igual a 20")
+    console.log("valor menor ou igual a 20");
 }
 //ex 05
+let higherNumber = numbers[0];
+for(let indexValue = 1; indexValue < numbers.length; indexValue += 1){
+    if(numbers[indexValue] > higherNumber){
+        higherNumber = numbers[indexValue]
+    }
+}
+console.log(higherNumber)
+//
