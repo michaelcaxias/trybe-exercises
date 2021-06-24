9101b4f8b914954bdbc9f80ea70e22c50d3abe5a
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-24 15:15:05 -0300

    Adiciona fixação de Arrays

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_2/fixacaoArrays.js
@@ -0,0 +1,15 @@
+//Fixação sobre Arrays
+//ex1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+//console.log(menuServices);
+
+//ex2
+let indexOfPortfolio = menu.indexOf('Portfólio');
+//console.log(indexOfPortfolio);
+
+//ex3
+menu.push('Contato')
+console.log(menu)
