8b0a0f6761a252f668d82f5fdc6a18e263e4cadb
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-24 19:17:08 -0300

    Exercicio 1 e 2 - Completos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
@@ -0,0 +1,8 @@
+//Exercicios JS
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//ex 02
+let sum = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    sum += numbers[index]
+    console.log(sum)
+}
