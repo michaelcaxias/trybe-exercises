8c9ba670935b90cf7d61bc12cde742c9cf0dfb2d
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-24 20:52:05 -0300

    Exercicio 9 - Completo

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
@@ -5,19 +5,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 for (let index = 0; index < numbers.length; index += 1) {
     sum += numbers[index];
- // console.log(sum);
 }
+console.log("A soma de tudo é: "+ sum);
 
 //ex 03
 let arithmeticAverage = sum / numbers.length;
-//console.log("A média aritmética é: " + arithmeticAverage);
+console.log("A média aritmética é: " + arithmeticAverage);
 
 //ex 04
 if(arithmeticAverage > 20){
-//    console.log("Valor aritmético é maior que 20");
+    console.log("Valor aritmético é maior que 20");
 }
 else{
-//    console.log("valor aritmético é menor ou igual a 20");
+    console.log("valor aritmético é menor ou igual a 20");
 }
 
 //ex 05
@@ -27,7 +27,7 @@ for(let indexValue = 1; indexValue < numbers.length; indexValue += 1){
         higherNumber = numbers[indexValue]
     }
 }
-//console.log("O maior número é: "+higherNumber)
+console.log("O maior número é: "+higherNumber)
 
 //ex 06
 let oddCount = 0;
@@ -38,10 +38,10 @@ for(let indexOdd = 0; indexOdd < numbers.length; indexOdd += 1){
 }
 
 if (oddCount === 0) {
-//    console.log("nenhum valor ímpar encontrado");
+    console.log("nenhum valor ímpar encontrado");
 }
 else {
-//    console.log("A quantidade de valores impares é: " + oddCount);
+    console.log("A quantidade de valores impares é: " + oddCount);
 }
 //ex 07
 let lowerNumber = numbers[0];
@@ -50,7 +50,7 @@ for(let indexValue = 1; indexValue < numbers.length; indexValue += 1){
         lowerNumber = numbers[indexValue]
     }
 }
-//console.log("O menor número é: "+ lowerNumber)
+console.log("O menor número é: "+ lowerNumber)
 
 //ex 08
 let listArray = [];
@@ -58,6 +58,11 @@ let listArray = [];
 for(indexArray = 1; indexArray <= 25; indexArray+=1){
     listArray.push(indexArray);
 }
-//console.log(listArray)
+console.log(listArray)
 
 //ex 09
+let division = 0;
+for(indexDivision = 0; indexDivision < listArray.length; indexDivision+=1){
+    division = listArray[indexDivision] / 2
+}
+console.log("O resultado da divisão por 2 é: " + division)
