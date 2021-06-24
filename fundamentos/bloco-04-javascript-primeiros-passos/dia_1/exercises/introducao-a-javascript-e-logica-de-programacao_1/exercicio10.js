bc5eceba2ecf460d6e683d6a36886217a5aa4785
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-23 21:35:54 -0300

    Exercicio 10 - Ajustando com if/else

--- fundamentos/bloco-04-javascript-primeiros-passos/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio10.js
+++ fundamentos/bloco-04-javascript-primeiros-passos/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio10.js
@@ -5,7 +5,12 @@ let impostoSobreOCusto = valorCusto * 20 / 100;
 let valorCustoTotal = valorCusto + impostoSobreOCusto
 let valorLucro = valorVenda - valorCustoTotal
 
+if (valorCusto >= 0 && valorVenda >= 0){
+    console.log("O imposto é de:", impostoSobreOCusto + "$")
+    console.log("O valor do custo total é de:", valorCustoTotal + "$")
+    console.log("O valor do lucro é de:", valorLucro + "$")
 
-console.log("O imposto é de:", impostoSobreOCusto + "$")
-console.log("O valor do custo total é de:", valorCustoTotal + "$")
-console.log("O valor do lucro é de:", valorLucro + "$")
+}
+else {
+    console.log("ERRO: os valores não podem ser abaixo ou igual a 0.")
+}
