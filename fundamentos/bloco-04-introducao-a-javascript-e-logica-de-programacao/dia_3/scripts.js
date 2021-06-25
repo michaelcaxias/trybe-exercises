7ffac7f7674c4af612bd34f7caf67487fcbe299d
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-25 19:20:41 -0300

    Adicionando na tela o numero do exercicio

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_3/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_3/scripts.js
@@ -1,4 +1,5 @@
 //ex01
+console.log("EXERCICIO 1")
 let n = 5;
 
 for (let index = 0; n > index; index +=1){
@@ -10,9 +11,11 @@ for (let index = 0; n > index; index +=1){
     console.log(line)
 }
 //ex02
+console.log("EXERCICIO 2")
 let line = "";
 for (let index = 0; n > index; index +=1){
     let asterisk = "*"
     line = line + asterisk
     console.log(line)
 }
+//ex03
