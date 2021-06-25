73bceb8741d166bbe487cb12dabbd585cc038f8c
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-25 19:11:54 -0300

    Adiciona exercicio 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_3/scripts.js
@@ -0,0 +1,18 @@
+//ex01
+let n = 5;
+
+for (let index = 0; n > index; index +=1){
+    let line = "";
+    for (let index2 = 0; n > index2; index2 +=1){
+        let asterisk = "*"
+        line = line + asterisk
+    }
+    console.log(line)
+}
+//ex02
+let line = "";
+for (let index = 0; n > index; index +=1){
+    let asterisk = "*"
+    line = line + asterisk
+    console.log(line)
+}
