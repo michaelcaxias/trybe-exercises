b7c0f43a2bad583f1ff1efcafcbe735e4a460046
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-25 21:43:18 -0300

    [Exercicio 2] Raciocinio entendido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_3/ex02.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_3/ex02.js
@@ -9,3 +9,10 @@ for (let index = 0; index < n; index +=1){
     line = line + symbol
     console.log(line)
 }
+
+// Definimos a variavel n que recebe um numero maior que 1
+// A partir disso criamos a variavel line vazia que armazenara o simbolo posteriormente
+// criamos a variavel symbol com o asteristico para usarmos posteriormente
+// criamos um loop que vai de 0 e continuara sendo repetido enqunato for menor que n
+// a cada repetiçao a variavel line soma com a variavel symbol, ou seja, a cada repetição
+// é acrescentado um simbolo, até que no final fique uma escada.
