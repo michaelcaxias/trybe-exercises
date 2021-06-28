5da144f7a1494bdcc3b50bd46224b9298e04f413
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-28 15:08:37 -0300

    Adicionando ex04

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_3/ex03.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_3/ex03.js
@@ -9,20 +9,16 @@ for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
 
     if (columnIndex < inputPosition) {
-
-     line = line + ' ';
-
-    } else {
-
-     line = line + symbol;
-
+      line = line + ' '
+    }
+    else {
+      line = line + symbol
     }
   }
-
-  console.log (line);
- line = '';
+  console.log(line);
+  line = '';
   inputPosition -= 1;
-};
+}
 
 // A cada repetição a variavel line é resetada e o inputPosition é decrementado, ou seja, o loop principal será repetido até zerar o inputPosition e quando isso acontecer, todos os espaços e asteristicos serão computados
 
