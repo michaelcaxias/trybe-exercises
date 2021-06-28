98749c19820c360c04ba14693aa990728f65215f
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-28 16:18:50 -0300

    [Exercises 4-4] Adiciona exercicios de fixação

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_4/fixacao.js
@@ -0,0 +1,36 @@
+// Fixação 1 - Objetos
+
+/* let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    fullName: 'Marta Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+};
+
+console.log('A jogadora ' + player.name + ' tem ' + player.age + ' anos de idade.');
+console.log('A jogadora ' + player.fullName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.')
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.')
+ */
+
+//Fixação 2 - For/in
+
+/* let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+for (let index in names) {
+    console.log('Olá, ' + names[index])
+}
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+for (let index in car) {
+    console.log(index, car[index])
+} */
+
