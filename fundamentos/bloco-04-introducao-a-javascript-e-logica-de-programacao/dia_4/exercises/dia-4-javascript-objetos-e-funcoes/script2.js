b08e2c9a688ba08fd88f0d8ae154bd4e2254de55
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-06-30 15:34:03 -0300

    [Exercicios 4-4] Nova forma de pegar o maior elemento

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_4/exercises/dia-4-javascript-objetos-e-funcoes/script2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_4/exercises/dia-4-javascript-objetos-e-funcoes/script2.js
@@ -12,19 +12,13 @@ function isPalindrome(string) {
         return false;
     }
 }
-console.log(isPalindrome('ana'))
+console.log(isPalindrome('anna'))
 //Exercicio 2 
 function highestNumber(array) {
-    let numberPosition = 0;
-    for (let index = 1; index < array.length; index+=1){
-        if (numberPosition < array[index]) {
-            numberPosition = array[index];
-        }
-        else {
-            numberPosition += 1
-        }
-    }
-    return numberPosition - 1
+    //max pegada por https://www.freecodecamp.org/news/three-ways-to-return-largest-numbers-in-arrays-in-javascript-5d977baa80a1/
+    //apply -> https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Function/Apply
+    return Math.max.apply(null, array)
+    //o argumento null pegará todos os índices do array e colocará dentro do parametro do math.max, como se fosse math.max(numbers[0], numbers[1]....)
 }
 console.log(highestNumber([2, 3, 6, 7, 10, 1]))
 //Exercicio 3
@@ -41,3 +35,7 @@ function lowestNumber(array) {
     return numberPosition
 }
 console.log(lowestNumber([2, 4, 6, 7, 10, 0, -3]))
+//Exercicio 4
+function maxCharacters(array) {
+    
+}
