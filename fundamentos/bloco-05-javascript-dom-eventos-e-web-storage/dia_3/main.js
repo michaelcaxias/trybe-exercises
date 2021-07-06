9809ce4fe2723783c59aabb12ead2d88bcf312fb
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-06 20:45:28 -0300

    [Fixação 5.3] doubeClick adicionado

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia_3/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia_3/main.js
@@ -29,7 +29,7 @@ redirecione para alguma página;
 function clickLink() {
   window.open("https://michaelcaxias.github.io/", "_blank")
 }
-myWebpage.addEventListener("ondblclick", clickLink)
+myWebpage.addEventListener("dblclick", clickLink)
 /* 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 a cor do mesmo; */
 function mouseOver(){
