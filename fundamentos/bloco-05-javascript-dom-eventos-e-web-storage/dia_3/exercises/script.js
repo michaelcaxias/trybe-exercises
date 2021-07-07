7558bcdcb0371e6d52b2179555e5acf436412f81
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-06 22:37:35 -0300

    [Exercicios 5.3] 1 e 2 completos!

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia_3/exercises/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia_3/exercises/script.js
@@ -13,4 +13,32 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+/* ----Exercicio 1---- */
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+function liDays() {
+  let ul = document.querySelector('#days');
+  for (let days of dezDaysList) {
+    let li = document.createElement('li');
+    li.className = 'day';
+    li.innerText = days;
+    ul.appendChild(li);
+    if (days === 24 || days === 25 || days === 31) {
+      li.classList.add('holiday');
+    }
+    if (days === 4 || days === 11 || days === 18 || days === 25) {
+      li.classList.add('friday');
+    }
+  }
+}
+liDays()
+/* ----Exercicio 2---- */
+let stringFeriados = 'Feriados'
+function holidays(string) {
+  let buttonFather = document.querySelector('.buttons-container');
+  let button = document.createElement('button');
+  button.id = 'btn-holiday';
+  button.innerText = string;
+  buttonFather.appendChild(button);
+}
+holidays(stringFeriados)
+/* ----Exercicio 3---- */
