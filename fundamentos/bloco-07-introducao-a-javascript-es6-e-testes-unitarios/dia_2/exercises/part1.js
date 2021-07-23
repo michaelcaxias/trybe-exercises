b695a95e0f3e3c18f059ab501252382b0f0bf751
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-23 18:54:20 -0300

    Exercicios 7.2 - 1.1 Completo

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia_2/exercises/part1.js
@@ -0,0 +1,60 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+/* 1 - Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701". */
+// Note que o parâmetro da função já está sendo passado na chamada da função.
+
+const customerInfo = (order) => {
+  let theDeliveryPerson = order.order.delivery.deliveryPerson;
+  let namePerson = order.name;
+  let phone = order.phoneNumber;
+  let addressPerson = Object.values(order.address);
+  return `Òlá ${theDeliveryPerson}, entrega para: ${namePerson} Telefone: ${phone}, ${addressPerson}".`
+
+}
+
+console.log(customerInfo(order));
+
+/* 2 - Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00." */
+// Modifique o nome da pessoa compradora.
+// Modifique o valor total da compra para R$ 50,00.
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+
+}
+
+orderModifier(order);
