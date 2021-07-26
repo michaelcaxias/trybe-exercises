6a54e47646cb0b3e795ab375e1d4f5cebaf6a022
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-26 19:11:20 -0300

    Exercicios 7.3 - Parte III Completa

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia_3/exercises/pt4.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+const expected = myFizzBuzz(60);
+const expected2 = myFizzBuzz(9);
+const expected3 = myFizzBuzz(25);
+const expected4 = myFizzBuzz(11);
+const expected5 = myFizzBuzz('abelha');
+
+assert.deepStrictEqual(expected, 'fizzbuzz');
+assert.deepStrictEqual(expected2, 'fizz');
+assert.deepStrictEqual(expected3, 'buzz');
+assert.deepStrictEqual(expected4, 11);
+assert.deepStrictEqual(expected5, false);
