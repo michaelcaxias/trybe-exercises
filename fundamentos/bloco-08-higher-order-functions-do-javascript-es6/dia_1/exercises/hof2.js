186a339be717ae5b55211515a26765da5729ea33
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-28 19:37:31 -0300

    Bloco 08: Dia 1 - Ex3

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia_1/exercises/hof2.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia_1/exercises/hof2.js
@@ -3,12 +3,7 @@ const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const raffle = (num, callback) => {
   const result = callback(1, 5);
   console.log(`O número sorteado foi ${result}!`)
-
-  if (num === result) {
-    return 'Parabéns você ganhou';
-  } else {
-    return 'Tente novamente';
-  }
+  return (num === result) ? 'Parabéns você ganhou!' : 'Tente novamente'
 }
 
 console.log(raffle(4, random))
--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia_1/exercises/hof3.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia_1/exercises/hof3.js
@@ -2,7 +2,24 @@ const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 const algumaCoisa = (correctAnswers, checkAnswers, callback) => {
+  let count = 0;
   for (let index = 0; index < checkAnswers.length; index += 1) {
-      
+    const actionReturn = callback(correctAnswers[index], checkAnswers[index]);
+    count += actionReturn;
+  }
+  return `O aluno acertou ${count} questões.`
+}
+
+const comparation = (correctAnswer, checkAnswer) => {
+  if (checkAnswer === correctAnswer) {
+    return 1
+  }
+  if (checkAnswer !== correctAnswer) {
+    return -0.5
+  }
+  if (checkAnswer === 'N.A') {
+    return 0
   }
 }
+
+console.log(algumaCoisa(RIGHT_ANSWERS, STUDENT_ANSWERS, comparation))
