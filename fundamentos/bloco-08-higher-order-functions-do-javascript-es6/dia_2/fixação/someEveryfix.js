7fa3087095e198dd77d19a1261b3bfc42243d6ce
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-29 19:29:25 -0300

    Bloco 08: Dia 2 - Fix3

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia_2/fixação/someEveryfix.js
@@ -0,0 +1,21 @@
+// ----- FIX 1 -----
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => arr.some((element) => element === name);
+
+console.log(hasName(names, 'Ana'))
+
+// ----- FIX 2 -----
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => arr.every((element) => element.age >= minimumAge);
+
+console.log(verifyAges(people, 18));
