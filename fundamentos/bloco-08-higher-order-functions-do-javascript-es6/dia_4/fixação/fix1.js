e9af5c39900206645671c1e07150d6c0fd359234
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-08-02 15:38:36 -0300

    Bloco 08: Dia 4 - Fix1 ajustado

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia_4/fixação/fix1.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia_4/fixação/fix1.js
@@ -1,5 +1,7 @@
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
-const isEven = numbers.filter((element) => element % 2 === 0);
-const sumAll = numbers.reduce((accumulator, number) => accumulator + number);
+const isEven = (element) => element % 2 === 0;
+const sumPair = (accumulator, number) => accumulator + number;
+const sumAll = numbers.filter(isEven).reduce(sumPair);
+
 console.log(sumAll);
