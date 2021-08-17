be772a77c9131eb182352c4e024444a4dc0c4e4d
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-08-17 19:33:48 -0300

    Bloco 10: Dia 1 - Parte I

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia_1/exercises/tests/functionsTests.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia_1/exercises/tests/functionsTests.test.js
@@ -36,3 +36,74 @@ describe('Função myRemove(arr, item)', () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual(output)
   })
 });
+
+describe('Função myRemoveWithoutCopy(arr, item)', () => {
+  it('Verifica se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    output = [1, 2, 4]
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual(output)
+  })
+  it('Verifica se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    output = [1, 2, 3, 4];
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual(output)
+  })
+  it('Verifica se o array passado por parâmetro sofreu alterações', () => {
+    output = [1, 2, 3, 4];
+    expect(myRemoveWithoutCopy([1, 2, 3, 4])).toEqual(output)
+  })
+  it('Verifica se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    output = [1, 2, 3, 4];
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual(output)
+  })
+});
+
+describe('Função myFizzBuzz(num)', () => {
+  it('Verifica se a chamada myFizzBuzz(15) retorna o nome esperado', () => {
+    output = 'fizzbuzz';
+    expect(myFizzBuzz(15)).toEqual(output)
+  })
+  it('Verifica se a chamada myFizzBuzz(9) retorna o nome esperado', () => {
+    output = 'fizz';
+    expect(myFizzBuzz(9)).toEqual(output)
+  })
+  it('Verifica se a chamada myFizzBuzz(25) retorna o nome esperado', () => {
+    output = 'buzz';
+    expect(myFizzBuzz(25)).toEqual(output)
+  })
+  it('Verifica se a chamada myFizzBuzz(17) retorna o nome esperado', () => {
+    output = 17;
+    expect(myFizzBuzz(17)).toEqual(output)
+  })
+  it("Verifica se a chamada myFizzBuzz('não é um número') retorna false", () => {
+    output = false;
+    expect(myFizzBuzz('não é um numero')).toEqual(output)
+  })
+});
+
+describe('Comparando dois objetos para verificar se são iguais', () => {
+
+  const obj1 = {
+    title: 'My Title',
+    description: 'My Description',
+  };
+  
+  const obj2 = {
+    description: 'My Description',
+    title: 'My Title',
+  };
+  
+  const obj3 = {
+    title: 'My Different Title',
+    description: 'My Description',
+  };
+
+  it('Verifica se o objeto 1 é igual ao objeto 2', () => {
+    expect(myFizzBuzz(obj1)).not.toEqual(obj2)
+  })
+  it('Verifica se o objeto 1 é igual ao objeto 3', () => {
+    expect(myFizzBuzz(obj1)).not.toEqual(obj3)
+  })
+  it('Verifica se o objeto 2 é igual ao objeto 3', () => {
+    expect(myFizzBuzz(obj2)).not.toEqual(obj3)
+  })
+
+});
