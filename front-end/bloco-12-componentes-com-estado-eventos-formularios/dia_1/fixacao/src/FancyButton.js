6d7f086a4ba2aefff2deb661d6673e801dd83708
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-09-02 20:38:35 -0300

    Bloco 12: Dia 1 - Fixação 4

--- front-end/bloco-12-componentes-com-estado-eventos-formularios/dia_1/fixacao/src/FancyButton.js
+++ front-end/bloco-12-componentes-com-estado-eventos-formularios/dia_1/fixacao/src/FancyButton.js
@@ -7,18 +7,27 @@ class FancyButton extends React.Component {
     this.state = {
       clickCounter: 0
     }
-    this.clickedButton = this.clickedButton.bind(this)
+    this.clickedButton = this.clickedButton.bind(this);
+    this.getColorButton = this.getColorButton.bind(this);
+  }
+
+  getColorButton(num) {
+    return (num % 2 === 0)? 'green' : 'white';
   }
 
   clickedButton(e) {
-    e.target.innerText = this.setState((stateBefore, _props) => ({
+    const click = e.target;
+    click.innerText = this.setState((stateBefore, _props) => ({
       clickCounter: stateBefore.clickCounter + 1
     }));
   }
 function
   render() {
     return (
-        <button onClick={this.clickedButton}>{this.state.clickCounter}</button>
+        <button onClick={this.clickedButton}
+        style={{ backgroundColor: this.getColorButton(this.state.clickCounter) }}>
+          {this.state.clickCounter}
+        </button>
   );
   }
 }
