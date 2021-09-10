85990b2546bd934c91978eee045aff4b8444614a
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-09-10 18:16:22 -0300

    Bloco 13: Dia 1 - Fixação 1

--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/fixacao/src/App.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/fixacao/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { Component }from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+ constructor(props){
+     super(props);
+     this.state = {
+       page: 1,
+       characters: [],
+     };
+     this.nextAndBack = this.nextAndBack.bind(this)
+  }
+
+  async fetchCharacters() {
+    const { page } = this.state;
+    const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
+    const json = await response.json();
+    this.setState({characters: json.results});
+  }
+
+  nextAndBack({ target: { name } }) {
+    if (name === 'back' && this.state.page > 1) {
+      this.setState(previousState => ({
+        page: previousState.page - 1
+      }))
+      console.log('back');
+    }
+    if (name === 'next') {
+      this.setState(previousState => ({
+        page: previousState.page + 1
+      }))
+      console.log('next');
+    }
+  }
+
+  componentDidMount() {
+    console.log('montou');
+    this.fetchCharacters();
+  }
+
+render() {
+  const { state: { characters }, nextAndBack } = this;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Ricky and Morty Characters:
+      </h1>
+      <button name="back" onClick={ nextAndBack }>Back</button>
+      <button name="next" onClick={ nextAndBack }>Next</button>
+      <div className="body">
+        {characters.map(({ name, image }) => {
+          return (
+            <div className="container" key={name}>
+              <h3>{name}</h3>
+              <img src={image} alt={name}/>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
+}
 }
 
 export default App;
