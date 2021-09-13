3391594ff2160f615ae576a8b1047f3afb27774d
Michael <michaelcaxias.dev@gmail.com>
2021-09-13 19:21:33 -0300

    Bloco 12: Dia 2 - Fix2

--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/App.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        <Route path="/" >
+          <Home />  
+        </Route>
       </BrowserRouter>
     );
   }
--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/components/Home.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/components/Home.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
-      <Route>
-        <div>
-          <h2>Home</h2>
-          <p> My awesome Home component </p>
-        </div>
-      </Route>
+      <div>
+        <h2>Home</h2>
+        <p> My awesome Home component </p>
+      </div>
     );
   }
 }
