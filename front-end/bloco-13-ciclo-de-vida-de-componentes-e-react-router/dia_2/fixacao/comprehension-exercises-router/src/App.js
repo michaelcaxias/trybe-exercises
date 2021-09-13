7a63d4a0191ea0226f77d70a13cfc8122ebba6a0
Michael <michaelcaxias.dev@gmail.com>
2021-09-13 19:25:23 -0300

    Bloco 12: Dia 2 - Fix6

--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/App.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
@@ -12,6 +13,9 @@ class App extends Component {
         </Route>
         <Route path="/about" >
           <About />  
+        </Route>
+        <Route path="/users" >
+          <Users />  
         </Route>
       </BrowserRouter>
     );
