85721455ef73cebbaf7c7913ca34a58cb257a824
Michael <michaelcaxias.dev@gmail.com>
2021-09-13 20:45:49 -0300

    Tentando instalar ESLINT

--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/App.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
@@ -11,15 +11,13 @@ class App extends Component {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/users">Users</Link>
-        <Route exact path="/" >
-          <Home />  
-        </Route>
-        <Route path="/about" >
-          <About />  
-        </Route>
-        <Route path="/users/:id" render={(props) => (
-           <Users {...props} greetingsMessage="Good Morning!" />
-        ) }/>
+        <Switch>
+          <Route path="/users/:id" render={(props) => (
+            <Users {...props} greetingsMessage="Good Morning!" />
+          ) }/>
+          <Route path="/about" component={ About } />
+          <Route path="/" component={ Home }/>
+        </Switch>
       </BrowserRouter>
     );
   }
--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/components/Users.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/components/Users.js
@@ -1,12 +1,12 @@
 // arquivo Users.js
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Users extends Component {
   render() {
     const { match: { params: { id } }, greetingsMessage } = this.props
     return (
       <div>
-        <h2>User { id }</h2>
+        <h2>User</h2>
         <p> { greetingsMessage }, My awesome User { id } component </p>
       </div>
     );
@@ -14,4 +14,3 @@ class Users extends Component {
 };
 
 export default Users;
-
