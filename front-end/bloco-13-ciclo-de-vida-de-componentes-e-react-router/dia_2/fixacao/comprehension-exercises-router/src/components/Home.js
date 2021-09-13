116f58fb9b1cda5091494298e6a353d8a5c9abf1
Michael <michaelcaxias.dev@gmail.com>
2021-09-13 19:18:04 -0300

    Bloco 12: Dia 2 - Ex1

--- front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/components/Home.js
+++ front-end/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia_2/fixacao/comprehension-exercises-router/src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>Home</h2>
-        <p> My awesome Home component </p>
-      </div>
+      <Route>
+        <div>
+          <h2>Home</h2>
+          <p> My awesome Home component </p>
+        </div>
+      </Route>
     );
   }
 }
