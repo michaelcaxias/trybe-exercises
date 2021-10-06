d67f94a13bd1733dea9de3dd19e0d7af0928b20c
Michael <michaelcaxias.dev@gmail.com>
2021-10-06 13:36:36 -0300

    Bloco 15.1: Exercício 1

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia_1/exercises/script.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia_1/exercises/script.js
@@ -1,4 +1,15 @@
-function criarCor() {
+const PREVIOUS_COLOR = document.querySelector('#previous');
+const NEXT_COLOR = document.querySelector('#next');
+const RANDOM_COLOR = document.querySelector('#random-color');
+const COLOR_VALUE = document.querySelector('#value');
+const CONTAINER = document.querySelector('#container');
+
+const ESTADO_INICIAL = {
+  colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
+  index: 0,
+};
+
+function newColor() {
   const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
   let cor = '#';
   const aleatorio = () => Math.floor(Math.random() * oneChar.length);
@@ -7,3 +18,42 @@ function criarCor() {
   }
   return cor;
 }
+
+function reducer (state = ESTADO_INICIAL, action) {
+  const colors = [...state.colors, action.color];
+  switch (action.type) {
+    case "PREVIOUS_COLOR":
+      return {
+        ...state,
+        index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
+      };
+    case "NEXT_COLOR":
+      return {
+        ...state,
+        index: state.index === state.colors.length - 1 ? 0 : state.index + 1,
+      };
+    case "RANDOM_COLOR":
+      return {
+        ...state,
+        colors,
+        index: colors.indexOf(action.color),
+      };
+    default:
+      return state;
+  }
+}
+
+const store = Redux.createStore(reducer);
+
+PREVIOUS_COLOR.addEventListener('click', () =>  store.dispatch({ type: 'PREVIOUS_COLOR' }))
+NEXT_COLOR.addEventListener('click', () =>  store.dispatch({ type: 'NEXT_COLOR' }))
+RANDOM_COLOR.addEventListener('click', () =>  store.dispatch({
+  type: 'RANDOM_COLOR',
+  color: newColor(),
+}))
+
+store.subscribe(() => {
+  const { colors, index } = store.getState();
+  COLOR_VALUE.innerText = colors[index];
+  CONTAINER.style.backgroundColor = colors[index];
+})
