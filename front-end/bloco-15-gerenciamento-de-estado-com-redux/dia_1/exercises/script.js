ef3b3b8f53314d4ddb027c690b55802920d2bb32
Michael <michaelcaxias.dev@gmail.com>
2021-10-06 11:50:21 -0300

    Bloco 15.1: Adiciona arquivos iniciais

---
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia_1/exercises/script.js
@@ -0,0 +1,9 @@
+function criarCor() {
+  const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+      cor += oneChar[aleatorio()];
+  }
+  return cor;
+}
