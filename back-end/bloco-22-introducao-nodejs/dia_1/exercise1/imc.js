aeae46ba714f5f0ee6b65d654404b2e8159f1296
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-07 16:05:53 -0300

    Bloco 22.1: Exercicio 4

--- back-end/bloco-22-introducao-nodejs/dia_1/exercise1/imc.js
+++ back-end/bloco-22-introducao-nodejs/dia_1/exercise1/imc.js
@@ -1,5 +1,7 @@
-const weight = 57;
-const height = 170;
+const readlineSync = require('readline-sync');
 
-const calculateBMI = () => console.log((weight / (height * height)) * 10000);
+const weight = parseInt(readlineSync.questionInt('Enter your weight in kg: '));
+const height = parseInt(readlineSync.questionInt('Enter your height in meters: '));
+
+const calculateBMI = () => console.log(parseInt((weight / (height * height)) * 10000));
 calculateBMI();
