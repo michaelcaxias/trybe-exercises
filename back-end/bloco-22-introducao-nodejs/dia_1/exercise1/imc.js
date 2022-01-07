eae9e77adc473a469a0da7231c1b1fc71ec14f5a
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-07 16:10:20 -0300

    Bloco 22.1: Exercício 5

--- back-end/bloco-22-introducao-nodejs/dia_1/exercise1/imc.js
+++ back-end/bloco-22-introducao-nodejs/dia_1/exercise1/imc.js
@@ -3,5 +3,17 @@ const readlineSync = require('readline-sync');
 const weight = parseInt(readlineSync.questionInt('Enter your weight in kg: '));
 const height = parseInt(readlineSync.questionInt('Enter your height in meters: '));
 
-const calculateBMI = () => console.log(parseInt((weight / (height * height)) * 10000));
+const calculateBMI = () => {
+    const bmi = parseInt((weight / (height * height)) * 10000);
+    console.log(`Your BMI is: ${bmi}`);
+    if (bmi < 18.5) {
+        console.log('You are underweight');
+    } else if (bmi < 25) {
+        console.log('You are normal weight');
+    } else if (bmi < 30) {
+        console.log('You are overweight');
+    } else {
+        console.log('You are obese');
+    }
+}
 calculateBMI();
