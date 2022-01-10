afc66ebb4a070c609dd8a3e45cee4c3c523ca79d
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-10 15:36:02 -0300

    Exercício 2: Concluído

--- back-end/bloco-22-introducao-nodejs/dia_2/exercises1/index.js
+++ back-end/bloco-22-introducao-nodejs/dia_2/exercises1/index.js
@@ -4,6 +4,10 @@ const typeOfFunction = (a, b, c) => {
   if (typeof c !== 'number') return true;
 }
 
+const shuffleNumber = () => {
+  return Math.floor(Math.random() * 101);
+}
+
 
 const promisse = (a, b, c) => {
   return new Promise((resolve, reject) => {
@@ -11,14 +15,18 @@ const promisse = (a, b, c) => {
       reject(new Error('Informe apenas números'));
     } else {
       const sum = (a + b) * c;
-      sum < 50 ? reject(new Error('Valor muito baixo')) : resolve(sum);
+      sum < 50 ? reject(new Error(`Valor muito baixo: ${sum}`)) : resolve(sum);
     }
   });
 };
 
 const resolvePromisse = async () => {
   try {
-    const result = await promisse(5, 2, 2);
+    const result = await promisse(
+      shuffleNumber(),
+      shuffleNumber(),
+      shuffleNumber()
+      );
     console.log(result);
   } catch (err) {
     console.log(err);
