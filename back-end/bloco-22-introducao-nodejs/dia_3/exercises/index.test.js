25f146816aceecf0e93900f24807ea4dcc500b3b
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-11 15:07:49 -0300

    Exercício 1: Concluído

---
+++ back-end/bloco-22-introducao-nodejs/dia_3/exercises/index.test.js
@@ -0,0 +1,16 @@
+const { expect } = require("chai")
+
+describe('Verifica se a função checkValue está funcionando corretamente', () => {
+  it('Verifica se a função gera um resultado positivo', () => {
+    expect(checkValue(1)).to.be.a('string');
+    expect(checkValue(1)).to.be.equal('positivo');
+  })
+  it('Verifica se a função gera um resultado negativo', () => {
+    expect(checkValue(-1)).to.be.a('string');
+    expect(checkValue(-1)).to.be.equal('negativo');
+  })
+  it('Verifica se a função gera um resultado neutro', () => {
+    expect(checkValue(0)).to.be.a('string');
+    expect(checkValue(0)).to.be.equal('neutro');
+  })
+})
