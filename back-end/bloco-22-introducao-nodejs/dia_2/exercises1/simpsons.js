9ce6a83a4cbf11dd877a7cc1a81e44594c05b34c
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-01-11 14:52:36 -0300

    Remove testes e pastas da trybe

--- back-end/bloco-22-introducao-nodejs/dia_2/exercises1/simpsons.js
+++ back-end/bloco-22-introducao-nodejs/dia_2/exercises1/simpsons.js
@@ -1,17 +1,17 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const simpsonsJSON = './simpsons.json';
+const simpsonsJSON = "./simpsons.json";
 // async
 const main = async () => {
-  const result = await fs.promises.readFile(simpsonsJSON, 'utf8');
+  const result = await fs.promises.readFile(simpsonsJSON, "utf8");
   const resolve = await JSON.parse(result);
 
   console.log(resolve);
   return resolve;
-}
+};
 
 main();
 
 // sync
-const simpsonsSync = JSON.parse(fs.readFileSync(simpsonsJSON, 'utf8'));
+const simpsonsSync = JSON.parse(fs.readFileSync(simpsonsJSON, "utf8"));
 console.log(simpsonsSync);
