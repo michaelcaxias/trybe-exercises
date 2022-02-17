bbd3d0569a93242509d3b2423db2a34295a50d93
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-02-17 15:33:06 -0300

    feat: adiciona solução

--- back-end/bloco-26-typescript/typescript-26-2/index.ts
+++ back-end/bloco-26-typescript/typescript-26-2/index.ts
@@ -1,26 +1,55 @@
-const readlineSync = require('readline-sync');
+import { exit } from "process";
+import Months from "./Months";
+import Seasons from "./Seasons";
 
-enum mounthsOfYear {
-  January = 1,
-  February = 2,
-  March = 3,
-  April = 4,
-  May = 5,
-  June = 6,
-  July = 7,
-  August = 8,
-  September = 9,
-  October = 10,
-  November = 11,
-  Decembe = 11
+const readlineSync = require("readline-sync");
+
+const monthsNames = Object.values(Months).
+    map(item => item);
+
+const choiceMonth = readlineSync.keyInSelect(monthsNames, "Escolha um mês do ano", { cancel: "SAIR" });
+
+if (choiceMonth === -1) {
+    console.log("Saindo!");
+    exit();
 }
 
-enum seasonsOfYear {
-  Winter = 1,
-  Spring = 2,
-  Summer = 3,
-  Autumn = 4
+const seasonsSouth = {
+  [Seasons.OUTONO]: [Months.MARCO, Months.ABRIL, Months.MAIO, Months.JUNHO],
+  [Seasons.INVERNO]: [Months.JUNHO, Months.JULHO, Months.AGOSTO, Months.SETEMBRO],
+  [Seasons.PRIMAVERA]: [Months.SETEMBRO, Months.OUTUBRO, Months.NOVEMBRO, Months.DEZEMBRO],
+  [Seasons.VERAO]: [Months.DEZEMBRO, Months.JANEIRO, Months.FEVEREIRO, Months.MARCO],
 }
 
-const month = readlineSync.question('Enter month: ');
-const season = readlineSync.question('Enter hemisphere: ');
+const seasonsNorth = {
+  [Seasons.PRIMAVERA]: seasonsSouth[Seasons.OUTONO],
+  [Seasons.VERAO]: seasonsSouth[Seasons.INVERNO],
+  [Seasons.OUTONO]: seasonsSouth[Seasons.PRIMAVERA],
+  [Seasons.INVERNO]: seasonsSouth[Seasons.VERAO],
+}
+
+const hemispheres = {
+  "Norte": seasonsNorth,
+  "Sul": seasonsSouth
+}
+
+const choiceHemisphere = readlineSync.keyInSelect(Object.keys(hemispheres), "Escolha um hemisfério", { cancel: "SAIR" });
+
+if (choiceHemisphere === -1) {
+  console.log("Saindo!");
+  exit();
+}
+
+const month = Object.values(Months)[choiceMonth];
+
+const hemisphere = Object.keys(hemispheres)[choiceHemisphere];
+
+console.log(`Mês: \n${month}`);
+console.log(`Hemisfério: \n${hemisphere}`);
+console.log(`Estações:`);
+const chosenHemisphereSeasons = Object.values(hemispheres)[choiceHemisphere]
+Object.entries(chosenHemisphereSeasons).map((entry) => {
+    const seasons = entry[0];
+    const months = entry[1];
+    if (months.includes(month)) console.log(seasons);
+});
