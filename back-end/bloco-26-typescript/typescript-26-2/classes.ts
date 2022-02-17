13a811901e46013c33494e4062c78dbe8a308d20
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-02-17 15:45:21 -0300

    feat: cria métodos para classe CAr

--- back-end/bloco-26-typescript/typescript-26-2/classes.ts
+++ back-end/bloco-26-typescript/typescript-26-2/classes.ts
@@ -1,5 +1,6 @@
 import Colors from "./Colors";
 import Doors from "./Doors";
+import Directions from "./Directions";
 
 class Car {
   brand: string;
@@ -16,6 +17,34 @@ class Car {
   }
 
   openTheDoor(door: Doors): void {
-    console.log("Abrindo porta");
+    console.log(`Abrindo porta ${door}`);
+  }
+
+  closeTheDoor(door: Doors): void {
+    console.log(`Fechando porta ${door}`);
+  }
+
+  turnOn(): void {
+    console.log("Ligando o carro");
+  }
+
+  turnOff(): void {
+    console.log("Desligando o carro");
+  }
+
+  speedUp(): void {
+    console.log("Acelerando o carro");
+  }
+
+  speedDown(): void {
+    console.log("Desacelerando o carro");
+  }
+
+  stop(): void {
+    console.log("Parando o carro");
+  }
+
+  turn(directions: Directions): void {
+    console.log(`Virando o carro para ${directions}`);
   }
 }
