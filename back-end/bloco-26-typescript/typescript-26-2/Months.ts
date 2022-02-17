96db88771ae656ca3685590caf6840981001a98a
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-02-17 15:23:24 -0300

    feat: separa enums em arquivos diferentes

---
+++ back-end/bloco-26-typescript/typescript-26-2/Months.ts
@@ -0,0 +1,17 @@
+
+enum MonthsOfYear {
+  JANEIRO = "Janeiro",
+  FEVEREIRO = "Fevereiro",
+  MARCO = "Março",
+  ABRIL = "Abril",
+  MAIO = "Maio",
+  JUNHO = "Junho",
+  JULHO = "Julho",
+  AGOSTO = "Agosto",
+  SETEMBRO = "Setembro",
+  OUTUBRO = "Outubro",
+  NOVEMBRO = "Novembro",
+  DEZEMBRO = "Dezembro",
+}
+
+export default MonthsOfYear;
