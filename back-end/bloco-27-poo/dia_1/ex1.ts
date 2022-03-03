a8bdc2455779f16cc37c0685e3d3083afb310550
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-03 19:32:41 -0300

    feat: adiciona funções para somar e mostrar a média // adiciona função sumNotes // adiciona função averageNotes

--- back-end/bloco-27-poo/dia_1/ex1.ts
+++ back-end/bloco-27-poo/dia_1/ex1.ts
@@ -1,12 +1,26 @@
 class Students {
   enroll: string;
   name: string;
-  testNote: number;
-  testWork: number;
-  constructor(enroll: string, name: string, testNote: number, testWork: number) {
+  testNote: number[];
+  testWork: number[];
+  constructor(enroll: string, name: string, testNote: number[], testWork: number[]) {
     this.enroll = enroll;
     this.name = name;
     this.testNote = testNote;
     this.testWork = testWork;
   }
+
+  sumNotes = () => {
+    const totalNotesTest = this.testNote.reduce((prev, curr) => prev + curr);
+    const totalNotesWork = this.testWork.reduce((prev, curr) => prev + curr);
+    return totalNotesTest + totalNotesWork;
+  }
+
+  averageNotes = () => {
+    const totalNotesTest = this.testNote.reduce((prev, curr) => prev + curr);
+    const totalNotesWork = this.testWork.reduce((prev, curr) => prev + curr);
+    const totalAllNotes = totalNotesTest + totalNotesWork;
+    return totalAllNotes / 6;
+  }
+
 }
