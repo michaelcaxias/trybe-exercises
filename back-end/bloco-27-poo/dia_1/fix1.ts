a281fc2b678f902fbdfd2ce4df557106c2389833
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-03 15:50:23 -0300

    feat: cria getters setters e altera visiblidade

--- back-end/bloco-27-poo/dia_1/fix1.ts
+++ back-end/bloco-27-poo/dia_1/fix1.ts
@@ -1,22 +1,39 @@
 class Tv {
-  brand: number;
-  resolution: string;
-  connections: string[];
-  connectedTo: string;
+  private _brand: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo: string;
   
   constructor(brand: number, resolution: string, connections: string[]) {
-    this.brand = brand;
-    this.resolution = resolution;
-    this.connections = connections;
+    this._brand = brand;
+    this._resolution = resolution;
+    this._connections = connections;
   }
 
   turnOn = () => {
-    const { brand, resolution, connections } = this;
-    console.log(brand);
-    console.log(resolution);
-    console.log(connections);
+    const { _brand, _resolution, _connections } = this;
+    console.log(_brand);
+    console.log(_resolution);
+    console.log(_connections);
   };
+
+  get connectedTo(): string {
+    return this._connectedTo;
+  }
+
+  set connectedTo(value: string) {
+    const isConnectionIncluded = this._connections.includes(value);
+    if (isConnectionIncluded) {
+      this._connectedTo = value;
+      console.log(this._connectedTo);
+    } else {
+      console.log('Sorry, connection unavaliable!')
+    }
+  }
+
+
 }
 
 const tv = new Tv(15, '1280x720', ['HDMI', 'Wifi']);
-console.log(tv.turnOn());
+tv.connectedTo = 'Wifi';
+console.log('Connected to:', tv.connectedTo);
