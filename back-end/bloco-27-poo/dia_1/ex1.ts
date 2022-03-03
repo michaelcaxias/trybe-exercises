8cbf5381d0ab2ec5da48aad1b4fb7debc6ebb3a2
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-03 19:15:02 -0300

    feat: inicializa classe // cria classe e constructor

---
+++ back-end/bloco-27-poo/dia_1/ex1.ts
@@ -0,0 +1,12 @@
+class Students {
+  enroll: string;
+  name: string;
+  testNote: number;
+  testWork: number;
+  constructor(enroll: string, name: string, testNote: number, testWork: number) {
+    this.enroll = enroll;
+    this.name = name;
+    this.testNote = testNote;
+    this.testWork = testWork;
+  }
+}
