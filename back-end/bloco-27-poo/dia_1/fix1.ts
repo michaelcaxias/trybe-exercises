bbddd1923f66272017e7d8db7284d7773a3d1958
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-03 15:15:00 -0300

    feat: inicializa exercicios do dia 1 // cria arquivos package.json e fix.ts

---
+++ back-end/bloco-27-poo/dia_1/fix1.ts
@@ -0,0 +1,20 @@
+class Tv {
+  brand: number;
+  resolution: string;
+  connections: string;
+
+  constructor(brand: number, resolution: string, connections: string) {
+    this.brand = brand;
+    this.resolution = resolution;
+    this.connections = connections;
+  }
+  turnOn = () => {
+    const { brand, resolution, connections } = this;
+    console.log(brand)
+    console.log(resolution)
+    console.log(connections)
+  };
+}
+
+const tv = new Tv(15, '1280x720', 'HDMI');
+console.log(tv.turnOn)
