75df83f68e068cb29b7db353c0a1786ffd9840b6
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-03 15:32:10 -0300

    fix: ajusta tipagem `connections`

--- back-end/bloco-27-poo/dia_1/fix1.ts
+++ back-end/bloco-27-poo/dia_1/fix1.ts
@@ -1,20 +1,22 @@
 class Tv {
   brand: number;
   resolution: string;
-  connections: string;
-
-  constructor(brand: number, resolution: string, connections: string) {
+  connections: string[];
+  connectedTo: string;
+  
+  constructor(brand: number, resolution: string, connections: string[]) {
     this.brand = brand;
     this.resolution = resolution;
     this.connections = connections;
   }
+
   turnOn = () => {
     const { brand, resolution, connections } = this;
-    console.log(brand)
-    console.log(resolution)
-    console.log(connections)
+    console.log(brand);
+    console.log(resolution);
+    console.log(connections);
   };
 }
 
-const tv = new Tv(15, '1280x720', 'HDMI');
-console.log(tv.turnOn)
+const tv = new Tv(15, '1280x720', ['HDMI', 'Wifi']);
+console.log(tv.turnOn());
