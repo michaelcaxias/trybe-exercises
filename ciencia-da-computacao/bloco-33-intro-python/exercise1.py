af06237cc2e5319e5363e1cd37407509598da679
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-18 21:20:14 -0300

    style: altera nome de variavel para inglês

--- ciencia-da-computacao/bloco-33-intro-python/exercise1.py
+++ ciencia-da-computacao/bloco-33-intro-python/exercise1.py
@@ -1,9 +1,9 @@
-numero1 = 10
-numero2 = 20
+number1 = 10
+number2 = 20
 
-print(f'Subtração: {numero1 - numero2}')
-print(f'Soma: {numero1 + numero2}')
-print(f'Potenciação: {numero1 ** numero2}')
-print(f'Divisão inteira: {numero1 // numero2}')
-print(f'Divisão flutuante: {numero1 / numero2}')
-print(f'Verifica se 15 está entre 10 e 20: {numero1 < 15 < numero2}')
+print(f'Subtração: {number1 - number2}')
+print(f'Soma: {number1 + number2}')
+print(f'Potenciação: {number1 ** number2}')
+print(f'Divisão inteira: {number1 // number2}')
+print(f'Divisão flutuante: {number1 / number2}')
+print(f'Verifica se 15 está entre 10 e 20: {number1 < 15 < number2}')
