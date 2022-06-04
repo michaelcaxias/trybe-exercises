8ec88d8e2dab86d00750460ad03f8cc2913ffaee
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-03 21:09:14 -0300

    :sparkles: feat: implementa classe geladeira

---
+++ ciencia-da-computacao/bloco-34-python-poo/1/exercicios/geladeira.py
@@ -0,0 +1,18 @@
+class Geladeira:
+    def __init__(self, cor, preco, marca):
+        self.__cor = cor
+        self.__ligado = False
+        self.__temperatura = 'quente'
+        self.preco = preco
+        self.marca = marca
+    
+    def ligar(self):
+        self.__ligado = True
+        self.__temperatura = 'frio'
+
+    def desligar(self):
+        self.__ligado = False
+        self.__temperatura = 'quente'
+
+    def esta_ligado(self):
+        return self.__ligado
