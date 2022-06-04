bf377eb36fc45ce0f3ff015608d0c0501660a001
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-03 21:04:25 -0300

    :sparkles: feat: implementa class pessoa

---
+++ ciencia-da-computacao/bloco-34-python-poo/1/exercicios/pessoa.py
@@ -0,0 +1,9 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        self.geladeira = False
+        self.notebook = True
+      
+    def comprar_geladeira(self, geladeira: Geladeira):
+        pass
